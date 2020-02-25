1dccfcce534bad0d2a3a24366a89cb5666566242
xiaohuoni <448627663@qq.com>
2020-02-25 14:45:49 +0800

    chore: code style

--- src/models/connect.d.ts
+++ src/models/connect.d.ts
@@ -29,14 +29,12 @@ export type Effect = (
  * @type P: Type of payload
  * @type C: Type of callback
  */
-export type Dispatch = <P = any, C = (payload: P) => void>(
-  action: {
-    type: string;
-    payload?: P;
-    callback?: C;
-    [key: string]: any;
-  },
-) => any;
+export type Dispatch = <P = any, C = (payload: P) => void>(action: {
+  type: string;
+  payload?: P;
+  callback?: C;
+  [key: string]: any;
+}) => any;
 
 export interface Loading {
   global: boolean;
